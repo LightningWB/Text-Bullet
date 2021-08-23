d4c8d0480a3d94c0a07b9a44a620ab1e2615c623
LightningWB <lightningwb@gmail.com>
2021-08-22 22:48:36 -0500

    changed to manual d.ts gen because everything else is designed for modules

--- build.js
+++ build.js
@@ -1,14 +1,6 @@
 (async function(){
 	// generate bullet.d.ts
 	const fs = require('fs');
-	const { Generator } = require('npm-dts');
-	await new Generator({
-		entry: './src/plugin.ts',
-		output: './dist/bullet.d.ts'
-	}).generate();
-	
-	let file = fs.readFileSync('./dist/bullet.d.ts');
-	file = file.toString().replace('declare module \'text-bullet/src/plugin\'', 'namespace bullet') + '\nexport = bullet';
-	fs.writeFileSync('./dist/bullet.d.ts', file);
 	fs.writeFileSync('./dist/bullet.js', 'module.export = global.bullet;');
+	fs.writeFileSync('./dist/bullet.d.ts', fs.readFileSync('./bullet.d.ts'));
 })();
