3cf1f167af40a085b25d6cc06ea4d1c131b5f64c
LightningWB <lightningwb@gmail.com>
2021-08-26 15:35:42 -0500

    fix typo in build process

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 (async function(){
 	// generate bullet.d.ts
 	const fs = require('fs');
-	fs.writeFileSync('./dist/bullet.js', 'module.export = global.bullet;');
+	fs.writeFileSync('./dist/bullet.js', 'module.exports = global.bullet;');
 	fs.writeFileSync('./dist/bullet.d.ts', fs.readFileSync('./bullet.d.ts'));
 })();
